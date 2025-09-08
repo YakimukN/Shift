a4b4188cca70e3606e4f5886f04e45d36e81f4f0
alexs <yakimuknastya.rr@gmail.com>
2025-09-08 17:03:08 +0300

    final version

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
         MyFile myFile = new MyFile();
         WorkersBase.sortDataFromFiles(myFile.getDataFromAllFiles());
 
-
         boolean sort = false;
+        boolean stat = false;
+        boolean outputInFile = false;
         String sortType = "";
-//        Statistic.fullFiles();
+
         for (String arg : args) {
             if (arg.startsWith("--")){
                 String[] str = arg.substring(2).split("=", 2);
@@ -19,25 +20,71 @@ public class Main {
                     } else if (str[1].equals("salary")) {
                         sort = true;
                     } else System.out.println("Неверный тип сортировки");
-                } else if (str[0].equals("order") && sort){
-                    if (str[1].equals("asc")){
-                        if (sortType.equals("name")){
-                            WorkersBase.sortEmployeeByName();
-                        } else WorkersBase.sortEmployeeBySalary();
-                    } else if (str[1].equals("desc")) {
-                        if (sortType.equals("name")){
-                            WorkersBase.sortEmployeeByNameReversed();
-                        } else WorkersBase.sortEmployeeBySalaryReversed();
-                    } else System.out.println("Неверно указан порядок сортировки");
+                } else if (str[0].equals("order")){
+                    if (sort){
+                        if (str[1].equals("asc")){
+                            if (sortType.equals("name")){
+                                WorkersBase.sortEmployeeByName();
+                            } else WorkersBase.sortEmployeeBySalary();
+                        } else if (str[1].equals("desc")) {
+                            if (sortType.equals("name")){
+                                WorkersBase.sortEmployeeByNameReversed();
+                            } else WorkersBase.sortEmployeeBySalaryReversed();
+                        } else {
+                            sort = false;
+                            System.out.println("Неверно указан порядок сортировки");
+                        }
+                    } else System.out.println("Нельзя указать порядок сортировки, без указания сортировки :(");
                 } else if (str[0].equals("stat")){
-                    System.out.println("Данный параметр находится на стадии доработки :)");
-                } else if (str[0].equals("output")){
-                    System.out.println("Данный параметр находится на стадии доработки :)");
-                }else if (str[0].equals("path")){
-                    System.out.println("Данный параметр находится на стадии доработки :)");
-                } else System.out.println("Нет таких параметров");
+                    if (str.length > 1){
+                        System.out.println("Параметр stat не принимает аргументов");
+                    } else {
+                        stat = true;
+                    }
+                } if (str[0].equals("output")){
+                    if (stat && str.length > 1){
+                        if (str[1].equals("console")){
+                            Statistic.showStatisticInConsole();
+                            stat = false;
+                        } else if (str[1].equals("file")) {
+                            outputInFile = true;
+                        }
+                    } else System.out.println("Неизвестный параметр");
+                } else if (str[0].equals("path")){
+                    if (outputInFile && str.length > 1){
+                        Statistic.fullStatisticFile(str[1]);
+                        outputInFile = false;
+                    }
+                } else if (stat && str[0].equals("stat")) {
+                    Statistic.showStatisticInConsole();
+                    stat = false;
+                }
+            } else if (arg.startsWith("-")){
+                String[] str = arg.substring(1).split("=", 2);
+                if (str[0].equals("s")){
+                    if (str[1].equals("name")){
+                        sort = true;
+                        sortType = "name";
+                    } else if (str[1].equals("salary")) {
+                        sort = true;
+                    } else System.out.println("Неверный тип сортировки");
+                } else if (str[0].equals("o")){
+                    if (stat && str.length > 1){
+                        if (str[1].equals("console")){
+                            Statistic.showStatisticInConsole();
+                            stat = false;
+                        } else if (str[1].equals("file")) {
+                            outputInFile = true;
+                        }
+                    } else System.out.println("Неизвестный параметр");
+                } else if (str[0].equals("path")){
+                    if (outputInFile && str.length > 1){
+                        Statistic.fullStatisticFile(str[1]);
+                        outputInFile = false;
+                    }
+                }
             }
-
         }
+        Statistic.fullFiles();
     }
 }
--- src/Statistic.java
+++ src/Statistic.java
@@ -1,16 +1,12 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class Statistic {
     private static Map<String, Integer> setDepartmentNames = new HashMap();
     private static Map<Employee, Boolean> notUsedEmployees = new HashMap<>();
 
     public static void fullFiles(){
-
-
         for (Manager manager: WorkersBase.getListManagers()){
             setDepartmentNames.put(manager.getDepartmentName().trim(), manager.getId());
         }
@@ -22,34 +18,64 @@ public class Statistic {
         }
 
         fullErrorFile();
-        fullStatisticFile();
     }
 
-    private static void fullStatisticFile() {
-        try (FileWriter fileWriter = new FileWriter("output\\statistic.txt")){
+    public static void showStatisticInConsole(){
+        List< String> statistic = getStatisticInfo();
+        System.out.println("department, min, max, mid");
+        for (String stat: statistic){
+            System.out.println(stat);
+        }
+    }
+
+    public static void fullStatisticFile(String filePath) {
+        List< String> statistic = getStatisticInfo();
+
+        try (FileWriter fileWriter = new FileWriter(filePath)){
             fileWriter.write("department, min, max, mid\n");
-            for (Map.Entry<String, Integer> data: setDepartmentNames.entrySet()){
-                int count = 0;
-                double min = Double.MAX_VALUE, max = 0;
-                for (Employee employee: WorkersBase.getListEmployee()){
-                    if (employee.getManagerId() == data.getValue()){
-                        count++;
-                        if (employee.getSalary() > max){
-                            max = employee.getSalary();
-                        }
-                        if (employee.getSalary() < min){
-                            min = employee.getSalary();
-                        }
-                    }
-                }
-                if (count == 0){
-                    min = 0;
-                }
-                fileWriter.write(data.getKey() + "," + min + "," + max + "," + String.format(Locale.US, "%.2f", (min + max) / 2) + "\n");
+            for (String stat: statistic){
+                fileWriter.write(stat + "\n");
             }
         } catch (IOException e) {
             System.out.println("Что-то пошло не так, мы работаем над ошибкой\n" + e);
         }
+    }
+
+    private static List<String> getStatisticInfo(){
+        List<String> statistic = new ArrayList<>();
+
+        for (Manager manager: WorkersBase.getListManagers()){
+            setDepartmentNames.put(manager.getDepartmentName().trim(), manager.getId());
+        }
+
+        for (Map.Entry<String, Integer> data: setDepartmentNames.entrySet()){
+            List<Employee> temp = new ArrayList<>();
+            for (Employee employee: WorkersBase.getListEmployee()){
+                if (employee.getManagerId() == data.getValue()){
+                    temp.add(employee);
+                }
+            }
+            double max = 0;
+            double min = Double.MAX_VALUE;
+            double mid = 0;
+            if (!temp.isEmpty()){
+                for (Employee employee: temp){
+                    double employeeSalary = employee.getSalary();
+                    mid += employeeSalary;
+                    if (employeeSalary > max){
+                        max = employeeSalary;
+                    }
+                    if (employeeSalary < min){
+                        min = employeeSalary;
+                    }
+                }
+                mid = mid / temp.size();
+            } else max = min = mid = 0;
+
+            statistic.add(data.getKey() + "," + max + "," + min + "," + mid);
+        }
+
+        return statistic;
     }
 
     public static void fullDepartmentFile(int managerId, String fileName){
